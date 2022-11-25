5dc8cfd9191bd48426ce0ea1473f6bbfdc692eb1
Harita Ravindranath <haritagr19@gmail.com>
2022-11-25 12:43:28 +0530

    implemented firestore

---
+++ src/FirebaseFirestoreService.js
@@ -0,0 +1,64 @@
+import firebase from "./FirebaseConfig";
+
+const firestore = firebase.firestore();
+
+const createDocument = (collection, document) => {
+  return firestore.collection(collection).add(document);
+};
+
+const readDocument = (collection, id) => {
+  return firestore.collection(collection).doc(id).get();
+};
+
+const readDocuments = async ({
+  collection,
+  queries,
+  orderByField,
+  orderByDirection,
+  perPage,
+  cursorId, //to keep track of what doc we are currently at
+}) => {
+  let collectionRef = firestore.collection(collection);
+
+  if (queries && queries.length > 0) {
+    for (const query of queries) {
+      collectionRef = collectionRef.where(
+        query.field,
+        query.condition,
+        query.value
+      );
+    }
+  }
+
+  if (orderByField && orderByDirection) {
+    collectionRef = collectionRef.orderBy(orderByField, orderByDirection);
+  }
+
+  if (perPage) {
+    collectionRef = collectionRef.limit(perPage);
+  }
+
+  if (cursorId) {
+    const document = await readDocument(collection, cursorId);
+    collectionRef = collectionRef.startAfter(document);
+  }
+
+  return collectionRef.get();
+};
+
+const updateDocument = (collection, id, document) => {
+  return firestore.collection(collection).doc(id).update(document);
+};
+
+const deleteDocument = (collection, id) => {
+  return firestore.collection(collection).doc(id).delete();
+};
+
+const FirebaseFirestoreService = {
+  createDocument,
+  readDocuments,
+  updateDocument,
+  deleteDocument,
+};
+
+export default FirebaseFirestoreService;
